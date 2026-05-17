65afbeab8cab25318dc3d24de8988b59d6ea2853
xiter00 <king@andyy.my.id>
2026-05-17 21:02:30 +0700

    Add files via upload

--- IPS1/IPS1.ino
+++ IPS1/IPS1.ino
@@ -88,7 +88,7 @@ void setup() {
   tft.fillScreen(ST77XX_BLACK);
 
   // Inisialisasi LittleFS
-  if (!LittleFS.begin()) {
+  if (!LittleFS.begin(true)) {
     Serial.println("Gagal memuat LittleFS!");
     tft.setTextSize(2);
     tft.setTextColor(ST77XX_RED);
